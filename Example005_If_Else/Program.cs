bfe2b608b7ad154c96168061888df9d2fda91983
Медведев Андрей <AndreyMedwedew3@gmail.com>
2023-01-02 18:10:08 +0300

    add If-Else

---
+++ Example005_If_Else/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Write your Name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("WoW, It s Masha!!!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.Write(username);
+}
